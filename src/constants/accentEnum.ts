86ccee06108261caf76cc42259c9b965c8e81ca0
Andrew Robertson <andrewcrobertson@gmail.com>
2020-12-08 09:40:51 +1100

    feat: add accent store

---
+++ src/constants/accentEnum.ts
@@ -0,0 +1,5 @@
+import type { AccentEnum } from './enums';
+
+export const australian: AccentEnum = 'australian';
+export const english: AccentEnum = 'english';
+export const unitedStates: AccentEnum = 'unitedStates';
---
+++ src/constants/enums.ts
@@ -0,0 +1,1 @@
+export type AccentEnum = 'australian' | 'english' | 'unitedStates';
