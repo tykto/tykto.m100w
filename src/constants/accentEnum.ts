93ce218b177ee97792488d754879fdcd63f5ced7
Andrew Robertson <andrewcrobertson@gmail.com>
2020-12-08 16:21:00 +1100

    refactor: tidy select accent

--- src/constants/accentEnum.ts
+++ src/constants/accentEnum.ts
@@ -1,5 +1,9 @@
-import type { AccentEnum } from './enums';
+// import type { AccentEnum } from './enums';
 
-export const australian: AccentEnum = 'australian';
-export const english: AccentEnum = 'english';
-export const unitedStates: AccentEnum = 'unitedStates';
+// export const australian: AccentEnum = 'australian';
+// export const english: AccentEnum = 'english';
+// export const unitedStates: AccentEnum = 'unitedStates';
+
+export const australian = 'australian';
+export const english = 'english';
+export const unitedStates = 'unitedStates';
--- src/pages/SelectAccent/index.svelte
+++ src/pages/SelectAccent/index.svelte
@@ -1,20 +1,21 @@
-<script lang="ts">
+<script type="ts">
+  import * as accent from '@app/constants/accentEnum';
   import { accentStore } from '@app/data/accentStore';
   import Button from './Button';
-  import flagAustraliaUrl from './australia-flag.gif';
-  import flagUKUrl from './uk-flag.gif';
-  import flagUSUrl from './us-flag.gif';
+  import flagAustraliaUrl from './assets/flag-australia.gif';
+  import flagEnglandUrl from './assets/flag-england.gif';
+  import flagUSUrl from './assets/flag-us.gif';
 
-  const handleClick = (e) => {
-    accentStore.set(e.detail);
+  const handleClick = ({ detail }: any) => {
+    accentStore.set(detail);
     window.location.href = '/colours';
   };
 </script>
 
 <div class="flex flex-auto flex-col justify-center items-center h-full">
   <div class="flex flex-col space-y-6 py-3">
-    <Button accentValue="australian" imageUrl={flagAustraliaUrl} imageAltText="Australian flag" on:click={handleClick}>Australian</Button>
-    <Button accentValue="english" imageUrl={flagUKUrl} imageAltText="UK flag" on:click={handleClick}>English</Button>
-    <Button accentValue="unitedStates" imageUrl={flagUSUrl} imageAltText="US flag" on:click={handleClick}>United States</Button>
+    <Button accentValue={accent.australian} imageUrl={flagAustraliaUrl} imageAltText="Australian flag" on:click={handleClick}>Australian</Button>
+    <Button accentValue={accent.english} imageUrl={flagEnglandUrl} imageAltText="UK flag" on:click={handleClick}>English</Button>
+    <Button accentValue={accent.unitedStates} imageUrl={flagUSUrl} imageAltText="US flag" on:click={handleClick}>United States</Button>
   </div>
 </div>
