3def115c4af9e1cc479bb3029ca855b0db482765
Andrew Robertson <andrewcrobertson@gmail.com>
2020-12-08 14:56:46 +1100

    refactor: move button from select accent into its own component

---
+++ src/pages/SelectAccent/Button/index.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+  export let imageUrl = null;
+  export let imageAltText = '';
+  export let accentValue = null;
+
+  const dispatch = createEventDispatcher();
+</script>
+
+<button
+  class="flex flex-col items-center bg-yellow-200 shadow hover:shadow-lg focus:outline-none focus:shadow-lg font-bold py-4 px-4"
+  on:click={() => dispatch('click', accentValue)}>
+  <img src={imageUrl} alt={imageAltText} />
+  <span class="text-2xl pt-2"><slot /></span>
+</button>
--- src/pages/SelectAccent/index.svelte
+++ src/pages/SelectAccent/index.svelte
@@ -1,34 +1,20 @@
 <script lang="ts">
   import { accentStore } from '@app/data/accentStore';
+  import Button from './Button';
   import flagAustraliaUrl from './australia-flag.gif';
   import flagUKUrl from './uk-flag.gif';
   import flagUSUrl from './us-flag.gif';
 
   const handleClick = (e) => {
-    accentStore.set(e);
+    accentStore.set(e.detail);
     window.location.href = '/colours';
   };
 </script>
 
 <div class="flex flex-auto flex-col justify-center items-center h-full">
   <div class="flex flex-col space-y-6 py-3">
-    <button
-      class="flex flex-col items-center bg-yellow-200 shadow hover:shadow-lg focus:outline-none focus:shadow-lg font-bold py-4 px-4"
-      on:click={() => handleClick('australian')}>
-      <img class="" src={flagAustraliaUrl} alt="Australian flag" />
-      <span class="text-2xl pt-2">Australian</span>
-    </button>
-    <button
-      class="flex flex-col items-center bg-yellow-200 shadow hover:shadow-lg focus:outline-none focus:shadow-lg font-bold py-4 px-4"
-      on:click={() => handleClick('english')}>
-      <img class="" src={flagUKUrl} alt="UK flag" />
-      <span class="text-2xl pt-2">English</span>
-    </button>
-    <button
-      class="flex flex-col items-center bg-yellow-200 shadow hover:shadow-lg focus:outline-none focus:shadow-lg font-bold py-4 px-4"
-      on:click={() => handleClick('unitedStates')}>
-      <img class="" src={flagUSUrl} alt="US flag" />
-      <span class="text-2xl pt-2">United States</span>
-    </button>
+    <Button accentValue="australian" imageUrl={flagAustraliaUrl} imageAltText="Australian flag" on:click={handleClick}>Australian</Button>
+    <Button accentValue="english" imageUrl={flagUKUrl} imageAltText="UK flag" on:click={handleClick}>English</Button>
+    <Button accentValue="unitedStates" imageUrl={flagUSUrl} imageAltText="US flag" on:click={handleClick}>United States</Button>
   </div>
 </div>
