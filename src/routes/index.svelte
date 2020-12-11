0e37537bc00cdf431e6b33ec6e3008f3019accd4
Andrew Robertson <andrewcrobertson@gmail.com>
2020-12-11 16:24:52 +1100

    feat: remove splash

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,10 @@
-<!--<script lang="ts" ✂prettier:content✂="CiAgaW1wb3J0IFNwbGFzaFBhZ2UgZnJvbSAnQHRoaXMvcGFnZXMvU3BsYXNoJzsK">{}</script><SplashPage /> -->
 <script lang="ts" context="module">
   import { get } from 'svelte/store';
   import { accentStore } from '@this/data/accentStore';
 
   export async function preload() {
-    console.log(get(accentStore));
     if ((process as any).browser) {
       window.location.href = get(accentStore) === null ? '/accent' : '/colours';
     }
   }
 </script>
-
-
